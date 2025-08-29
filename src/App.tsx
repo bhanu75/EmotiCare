84a73964a5ee4324c4d570e3b25e3cbe99e57ea3
Bhanu Tripathi <26444713+bhanu75@users.noreply.github.com>
2025-08-29 14:47:32 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Good Morning 🌞</h1>
+      <p>Welcome to Morning Emotional Care</p>
+    </div>
+  );
+}
+
+export default App;
